99575f2661a3122485a8ebee846b65173503252d
Juliana Elias Tocci <juliana.tocci@al.infnet.edu.br>
2025-10-23 11:08:44 -0300

    alterando porta

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app=Flask(__name__)
 
@@ -33,5 +34,6 @@ def add():
     return 'Usuário adicionado com sucesso! <a href="/">Voltar</a>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host = '0.0.0.0', port = port)
 
